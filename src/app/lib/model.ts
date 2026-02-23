77df6b3ecdeaba9c0f6236f9896b1a4a2e7b115e
Coleman Escue <99232536+Nam3loc@users.noreply.github.com>
2026-02-23 14:50:09 -0600

    Create seed data and highlands event model

---
+++ src/app/lib/model.ts
@@ -0,0 +1,9 @@
+export interface HighlandsEvent {
+  	id: number;
+    name: string;
+    beginDate: Date;
+    endDate: Date;
+    description: string;
+    cost: number;
+	isPublished: boolean;
+}
